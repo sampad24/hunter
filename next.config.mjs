7e8b755985d9f9abfc967ecd6e0e97d6750d5d39
sampad24 <96785596+sampad24@users.noreply.github.com>
2025-11-14 05:14:40 +0530

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
- 
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/vfx',
+  assetPrefix: '/vfx',
+  trailingSlash: true,
 }
- 
+
 export default nextConfig
