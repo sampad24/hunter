55789daff856f70b2688d5af691fa15e4f9b97c1
sampad24 <96785596+sampad24@users.noreply.github.com>
2025-11-14 07:05:32 +0530

    Remove large gif files

---
+++ app/components/works/images.jsx
@@ -0,0 +1,78 @@
+export function W1() {
+  return (
+    <img
+      src="/works/w1.gif"
+      alt="GIF 1"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W2() {
+  return (
+    <img
+      src="/works/w2.gif"
+      alt="GIF 2"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W3() {
+  return (
+    <img
+      src="/works/w3.gif"
+      alt="GIF 3"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W4() {
+  return (
+    <img
+      src="/works/w4.gif"
+      alt="GIF 4"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W5() {
+  return (
+    <img
+      src="/works/w5.gif"
+      alt="GIF 5"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W6() {
+  return (
+    <img
+      src="/works/w6.gif"
+      alt="GIF 6"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+
+export function W7() {
+  return (
+    <img
+      src="/works/w7.gif"
+      alt="GIF 7"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
+export function W8() {
+  return (
+    <img
+      src="/works/w8.gif"
+      alt="GIF 8"
+      style={{ width: "100%", height: "auto" }}
+    />
+  );
+}
